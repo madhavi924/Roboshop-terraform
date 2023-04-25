33e9f8abe48de1cc8714ac79feaa5687f6e1edb8
madhavi <madhavilatha.008@gmail.com>
2023-04-25 12:29:58 -0500

    terraform

--- severs.tf
+++ severs.tf
@@ -1,3 +1,8 @@
+data "aws_ami" "centos" {
+  Owner = {"973714476881"}
+  most_recent =true
+  name_regex = "Centos-8-Devops-Practice"
+}
 resource "aws_instance" "frontend" {
   ami           = "ami-0b5a2b5b8f2be4ec2"
   instance_type = "t3.micro"
