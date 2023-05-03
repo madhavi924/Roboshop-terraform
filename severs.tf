21cbc168584de13ca646fb278d3a0c7197de1bfc
madhavi <madhavilatha.008@gmail.com>
2023-05-03 17:16:43 -0500

    terraform

--- severs.tf
+++ severs.tf
@@ -3,14 +3,14 @@ data "aws_ami" "centos" {
   most_recent = true
   name_regex = "Centos-8-DevOps-Practice"
 }
-data"aws_security_group" "allow-all" {
+data "aws_security_group" "allow-all" {
   name = "allow-all"
 }
 
   variable"component" {
     default = {
       frontend = {
-    name       = "frontend"
+      name = "frontend"
     instance_type = "t3.micro"
   }
       mongodb = {
