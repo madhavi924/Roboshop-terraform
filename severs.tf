89b628bc8d6f7daffb1bbe8cd3234b9e088dd1e8
madhavi <madhavilatha.008@gmail.com>
2023-05-04 14:31:43 -0500

    terraform

--- severs.tf
+++ severs.tf
@@ -1,13 +1,4 @@
-data "aws_ami" "centos" {
-  owners = ["973714476881"]
-  most_recent = true
-  name_regex = "Centos-8-DevOps-Practice"
-}
-data "aws_security_group" "allow-all" {
-  name = "allow-all"
-}
 
-  variable"component" {}
 
 resource "aws_instance" "instance" {
 for_each = var.component
