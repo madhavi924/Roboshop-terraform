0a0102f8c48c1c21d7b542ae810953c6ac7a5866
madhavi <madhavilatha.008@gmail.com>
2023-05-04 15:15:07 -0500

    terraform

--- severs.tf
+++ severs.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "instance" {
-for_each = var.component
-ami = data.aws_ami.centos.image_id
-instance_type = each.value["instance_type"]
-vpc_security_group_ids=[data.aws_security_group.allow-all.id]
-tags = {
-  name = each.value["name"]
-}
+  for_each               = var.component
+  ami                    = data.aws_ami.centos.image_id
+  instance_type          = each.value["instance_type"]
+  vpc_security_group_ids = [data.aws_security_group.allow-all.id]
+  tags                   = {
+    name = each.value["name"]
+  }
 }
