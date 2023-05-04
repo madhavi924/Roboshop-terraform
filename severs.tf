8c4b799584e8627e41940a632bda1a68c2752c70
madhavi <madhavilatha.008@gmail.com>
2023-05-04 14:23:21 -0500

    terraform

--- severs.tf
+++ severs.tf
@@ -7,50 +7,8 @@ data "aws_security_group" "allow-all" {
   name = "allow-all"
 }
 
-  variable"component" {
-    default = {
-      frontend = {
-        name          = "frontend"
-        instance_type = "t3.micro"
-      }
-      mongodb = {
-        name          = "mongodb"
-        instance_type = "t3.micro"
-      }
-      catalogue = {
-        name          = "catalogue"
-        instance_type = "t3.micro"
-      }
-      redis = {
-        name          = "redis"
-        instance_type = "t3.micro"
-      }
-      user = {
-        name          = "user"
-        instance_type = "t3.micro"
-      }
-      cart = {
-        name          = "cart"
-        instance_type = "t3.micro"
-      }
-      mysql = {
-        name          = "mysql"
-        instance_type = "t3.micro"
-      }
-      rabbitmq = {
-        name          = "rabbitmq"
-        instance_type = "t3.micro"
-      }
-      shipping = {
-        name          = "shipping"
-        instance_type = "t3.micro"
-      }
-      payment = {
-        name          = "payment"
-        instance_type = "t3.micro"
-      }
-    }
-  }
+  variable"component" {}
+
 resource "aws_instance" "instance" {
 for_each = var.component
 ami = data.aws_ami.centos.image_id
