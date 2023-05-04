f46c0f9ea6b0a662aa610d08f127a124966c32d9
madhavi <madhavilatha.008@gmail.com>
2023-05-03 20:45:39 -0500

    terraform

--- severs.tf
+++ severs.tf
@@ -10,45 +10,47 @@ data "aws_security_group" "allow-all" {
   variable"component" {
     default = {
       frontend = {
-      name = "frontend"
-    instance_type = "t3.micro"
-  }
-      mongodb = {
-      name = "mongodb"
-      instance_type = "t3.micro"
+        name          = "frontend"
+        instance_type = "t3.micro"
       }
-catalogue = {
-  name = "catalogue"
-  instance_type = "t3.micro"
-}
-redis = {
-  name = "redis"
-  instance_type = "t3.micro"
-}
-user = {
-  name = "user"
-  instance_type = "t3.micro"
-}
-cart = {
-  name = "cart"
-  instance_type = "t3.micro"
-}
-mysql = {
-  name = "mysql"
-  instance_type = "t3.micro"
-}
-rabbitmq = {
-  name = "rabbitmq"
-  instance_type = "t3.micro"
-}
-shipping = {
-  name = "shipping"
-  instance_type = "t3.micro"
-}
-payment = {
-  name = "payment"
-  instance_type = "t3.micro"
-}
+      mongodb = {
+        name          = "mongodb"
+        instance_type = "t3.micro"
+      }
+      catalogue = {
+        name          = "catalogue"
+        instance_type = "t3.micro"
+      }
+      redis = {
+        name          = "redis"
+        instance_type = "t3.micro"
+      }
+      user = {
+        name          = "user"
+        instance_type = "t3.micro"
+      }
+      cart = {
+        name          = "cart"
+        instance_type = "t3.micro"
+      }
+      mysql = {
+        name          = "mysql"
+        instance_type = "t3.micro"
+      }
+      rabbitmq = {
+        name          = "rabbitmq"
+        instance_type = "t3.micro"
+      }
+      shipping = {
+        name          = "shipping"
+        instance_type = "t3.micro"
+      }
+      payment = {
+        name          = "payment"
+        instance_type = "t3.micro"
+      }
+    }
+  }
 resource "aws_instance" "instance" {
 for_each = var.components
 ami = data.aws_ami.centos.image_id
