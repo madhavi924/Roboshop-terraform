e9081d414a96ae9568758c49e5fd2e25abd0f5bf
madhavi <madhavilatha.008@gmail.com>
2023-05-15 11:59:48 -0500

    ansible

--- module/locals.tf
+++ module/locals.tf
@@ -1,5 +1,5 @@
 locals {
-  name = var.env != " " ? "{var.component_nmae}-${var.env}" : var.component_name
+  name = var.env != " " ? "{var.component_name}-${var.env}" : var.component_name
   db_commands = [
   "rm rf roboshop-shell",
   "git clone https://github.com/madhavi924/roboshop-shell",
